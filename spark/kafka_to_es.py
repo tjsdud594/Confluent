3304ce2b1ba4bcc6de96ccb8debfe50021c97924
류선영 <84279479+tjsdud594@users.noreply.github.com>
2024-05-19 21:24:36 +0900

    add : spark_streaming으로 kafka topic접근

---
+++ spark/kafka_to_es.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+spark = SparkSession \
+        .builder \
+        .appName("readfromkafka") \
+        .master("local[*]") \
+        .getOrCreate()
+
+kafka_bootstrap_servers = "172.31.9.181:9092"
+topic_name = "worldmoney.filebeat"
+
+print("=================================Start Read Kafka Topic=================================")
+df = spark \
+  .readStream\
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+  .option("subscribe", topic_name) \
+  .load()
+print("=================================End Read Kafka Topic=================================")
+
+print(df.printSchema())
+
+query = df.writeStream \
+        .format("console") \
+        .outputMode("complete") \
+        .start()
+
+query.awaitTermination()
+
+# query = df.writeStream \
+# .outputMode("append") \
+# .queryName("writing_to_es") \
+# .format("org.elasticsearch.spark.sql") \
+# .option("checkpointLocation", "/tmp/") \
+# .option("es.resource", "index/type") \
+# .option("es.nodes", "localhost") \
+# .start()
